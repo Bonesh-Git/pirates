fe2fd2096c8c3cd04c120cbc1445286c9f9d6626
mohammadalishah1 <mohammadalishah700@gmail.com>
2025-05-29 19:24:01 +0330

    M Wallet

---
+++ Mohammad Wallet/Wallet/src/main/java/com/example/wallet/carrier/user/ExceptionCarrier.java
@@ -0,0 +1,4 @@
+package com.example.wallet.carrier.user;
+
+public record ExceptionCarrier(int code, String message) {
+}
---
+++ Mohammad Wallet/Wallet/src/main/java/com/example/wallet/exception/TransactionNotAllowedException.java
@@ -0,0 +1,28 @@
+package com.example.wallet.exception;
+
+public class TransactionNotAllowedException extends RuntimeException {
+    private int code;
+    private String message;
+
+    public TransactionNotAllowedException(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
